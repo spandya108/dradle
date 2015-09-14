2079834d0621880be6a47fb88e6e52178eca6486
Sahil Pandya <sahil.pandya108@gmail.com>
2015-09-14 01:16:22 -0700

    added goal, category, budget models and made transaction abstract--registered all with admin site

--- vbank/admin.py
+++ vbank/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Transaction, VirtualTransaction
+from .models import (
+    PhysicalTransaction,
+    VirtualTransaction,
+    Budget,
+    Goal,
+    Category
+)
 
 # Register your models here.
-admin.site.register(Transaction)
+admin.site.register(PhysicalTransaction)
 admin.site.register(VirtualTransaction)
+admin.site.register(Budget)
+admin.site.register(Goal)
+admin.site.register(Category)
--- vbank/models.py
+++ vbank/models.py
@@ -10,36 +10,60 @@ class Transaction(models.Model):
     credit          = models.IntegerField(default=0)
     debit           = models.IntegerField(default=0)
     combined        = models.BooleanField(default=False)
-    v_transaction   = models.ForeignKey('self', null=True, blank=True)
     date            = models.DateTimeField(default=None, blank=True)
     month           = models.DateField(default=None, blank=True)
     type            = models.CharField(default='', blank=True, max_length=100)
     institution     = models.CharField(default='', blank=True, max_length=100)
+    budgets         = models.ManyToManyField('Budget', blank=True)
+    goals           = models.ManyToManyField('Goal', blank=True)
+    categories      = models.ManyToManyField('Category', blank=True)
+    title           = models.CharField(max_length=100)
+    description     = models.TextField(null=True)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return '[-' + credit + '][+' + debit + ']'
 
-    def combine_transactions(self, transactions):
-        # if(not transactions):
-        #     return False
-        # else:
-        #     if(self.virtual == False):
-        #         self.combined = True
 
-        #         virtual_txn = Transaction(
-        #             id = uuid.uuid4(),
+class PhysicalTransaction(Transaction):
 
-        #         )
-        pass
+    v_transaction   = models.ForeignKey('VirtualTransaction', null=True, blank=True)
 
-class VirtualTransaction(models.Model):
+
+class VirtualTransaction(Transaction):
+
+    name            = models.CharField(null=False, max_length=100)
+    count           = models.IntegerField(default=1)
+
+
+class Budget(models.Model):
 
     id              = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    credit          = models.IntegerField(default=0)
-    debit           = models.IntegerField(default=0)
-    combined        = models.BooleanField(default=False)
-    date            = models.DateTimeField(default=None, blank=True)
-    month           = models.DateField(default=None, blank=True)
-    type            = models.CharField(default='', blank=True, max_length=100)
-    institution     = models.CharField(default='', blank=True, max_length=100)
+    amount          = models.IntegerField(default=0)
+    remaining       = models.IntegerField(default=0)
+    name            = models.CharField(default='New Budget', max_length=100, null=False)
+    p_transactions  = models.ManyToManyField('PhysicalTransaction', blank=True)
+    v_transactions  = models.ManyToManyField('VirtualTransaction', blank=True)
+    category        = models.ForeignKey('Category', null=True)
 
+
+
+class Goal(models.Model):
+
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    start_date      = models.DateField(blank=False)
+    end_date        = models.DateField(blank=True, default='')
+    amount          = models.IntegerField(default=0)
+    name            = models.CharField(default='New Goal', max_length=100, null=False)
+    p_transactions  = models.ManyToManyField('PhysicalTransaction', blank=True)
+    v_transactions  = models.ManyToManyField('VirtualTransaction', blank=True)
+
+
+class Category(models.Model):
+
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    p_transactions  = models.ManyToManyField('PhysicalTransaction', blank=True)
+    v_transactions  = models.ManyToManyField('VirtualTransaction', blank=True)
+    name            = models.CharField(default='Category', max_length=100, null=False)
